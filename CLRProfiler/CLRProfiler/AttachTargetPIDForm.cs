fdb260667185b6ec520613627d0f4ed8adf684b5
David Bremner <dbremner@gmail.com>
2017-07-15 14:08:12 -0700

    split up a few forms

--- CLRProfiler/CLRProfiler/AttachTargetPIDForm.cs
+++ CLRProfiler/CLRProfiler/AttachTargetPIDForm.cs
@@ -1,4 +1,4 @@
-﻿// ==++==
+// ==++==
 // 
 //   Copyright (c) Microsoft Corporation.  All rights reserved.
 // 
@@ -16,16 +16,8 @@ namespace CLRProfiler
     /// <summary>
     /// Summary description for AttachTargetPIDForm.
     /// </summary>
-    public class AttachTargetPIDForm : System.Windows.Forms.Form
+    public partial class AttachTargetPIDForm : System.Windows.Forms.Form
     {
-        private System.Windows.Forms.Button cancelButton;
-        private System.Windows.Forms.Button okButton;
-        private System.Windows.Forms.TextBox PIDtextBox;
-        private System.Windows.Forms.Label label1;
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.Container components = null;
 
         public AttachTargetPIDForm()
         {
@@ -55,90 +47,6 @@ namespace CLRProfiler
             }
             return pid;
         }
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                if (components != null)
-                {
-                    components.Dispose();
-                }
-            }
-            base.Dispose(disposing);
-        }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.cancelButton = new System.Windows.Forms.Button();
-            this.okButton = new System.Windows.Forms.Button();
-            this.PIDtextBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // cancelButton
-            // 
-            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(168, 104);
-            this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(75, 23);
-            this.cancelButton.TabIndex = 2;
-            this.cancelButton.Text = "Cancel";
-            // 
-            // okButton
-            // 
-            this.okButton.DialogResult = System.Windows.Forms.DialogResult.Yes;
-            this.okButton.Location = new System.Drawing.Point(27, 104);
-            this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(88, 23);
-            this.okButton.TabIndex = 1;
-            this.okButton.Text = "OK";
-            // 
-            // PIDtextBox
-            // 
-            this.PIDtextBox.Location = new System.Drawing.Point(127, 44);
-            this.PIDtextBox.Name = "PIDtextBox";
-            this.PIDtextBox.Size = new System.Drawing.Size(133, 20);
-            this.PIDtextBox.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 47);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 13);
-            this.label1.TabIndex = 9;
-            this.label1.Text = "Attach Target PID:";
-            // 
-            // AttachTargetPIDForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(288, 163);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.PIDtextBox);
-            this.Controls.Add(this.cancelButton);
-            this.Controls.Add(this.okButton);
-            this.Name = "AttachTargetPIDForm";
-            this.Text = "Attach Target";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-            this.AcceptButton = this.okButton;
-            this.CancelButton = this.cancelButton;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-
-        }
-
-        #endregion
 
     }
 }
--- CLRProfiler/CLRProfiler/CommentRangeForm.cs
+++ CLRProfiler/CLRProfiler/CommentRangeForm.cs
@@ -14,18 +14,8 @@ namespace CLRProfiler
     /// <summary>
     /// Summary description for CommentRangeForm.
     /// </summary>
-    public class CommentRangeForm : System.Windows.Forms.Form
+    public partial class CommentRangeForm : System.Windows.Forms.Form
     {
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button okButton;
-        private System.Windows.Forms.Button cancelButton;
-        private System.Windows.Forms.ComboBox startComboBox;
-        private System.Windows.Forms.ComboBox endComboBox;
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.Container components = null;
 
         internal const string startCommentString = "Start of Application";
         internal const string shutdownCommentString = "Shutdown of Application";
@@ -64,104 +54,6 @@ namespace CLRProfiler
 
             FillComboBoxes();
         }
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        protected override void Dispose( bool disposing )
-        {
-            if( disposing )
-            {
-                if(components != null)
-                {
-                    components.Dispose();
-                }
-            }
-            base.Dispose( disposing );
-        }
-
-        #region Windows Form Designer generated code
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.startComboBox = new System.Windows.Forms.ComboBox();
-            this.endComboBox = new System.Windows.Forms.ComboBox();
-            this.okButton = new System.Windows.Forms.Button();
-            this.cancelButton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // startComboBox
-            // 
-            this.startComboBox.Location = new System.Drawing.Point(128, 32);
-            this.startComboBox.Name = "startComboBox";
-            this.startComboBox.Size = new System.Drawing.Size(368, 21);
-            this.startComboBox.TabIndex = 0;
-            this.startComboBox.Text = "Start of Application";
-            // 
-            // endComboBox
-            // 
-            this.endComboBox.Location = new System.Drawing.Point(128, 88);
-            this.endComboBox.Name = "endComboBox";
-            this.endComboBox.Size = new System.Drawing.Size(368, 21);
-            this.endComboBox.TabIndex = 1;
-            this.endComboBox.Text = "Shutdown of Application";
-            // 
-            // okButton
-            // 
-            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.okButton.Location = new System.Drawing.Point(552, 32);
-            this.okButton.Name = "okButton";
-            this.okButton.TabIndex = 2;
-            this.okButton.Text = "OK";
-            this.okButton.Click += new System.EventHandler(this.okButton_Click);
-            // 
-            // cancelButton
-            // 
-            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(552, 88);
-            this.cancelButton.Name = "cancelButton";
-            this.cancelButton.TabIndex = 3;
-            this.cancelButton.Text = "Cancel";
-            // 
-            // label1
-            // 
-            this.label1.Location = new System.Drawing.Point(32, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 23);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Range starts at:";
-            // 
-            // label2
-            // 
-            this.label2.Location = new System.Drawing.Point(32, 88);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 23);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Range ends at:";
-            // 
-            // CommentRangeForm
-            // 
-            this.AcceptButton = this.okButton;
-            this.CancelButton = this.cancelButton;
-            this.ClientSize = new System.Drawing.Size(672, 150);
-            this.Controls.AddRange(new System.Windows.Forms.Control[] {
-                                                                          this.label2,
-                                                                          this.label1,
-                                                                          this.cancelButton,
-                                                                          this.okButton,
-                                                                          this.endComboBox,
-                                                                          this.startComboBox});
-            this.Name = "CommentRangeForm";
-            this.Text = "Select Range...";
-            this.ResumeLayout(false);
-
-        }
-        #endregion
 
         internal int startTickIndex;
         internal int endTickIndex;
