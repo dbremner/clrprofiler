61e63e9a9156db3194156bddeb6ed8dd5e27247e
David Bremner <dbremner@gmail.com>
2017-07-15 15:16:18 -0700

    split last form

---
+++ CLRProfiler/CLRProfiler/DiffCallTreeForm.Designer.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+using Microsoft.Win32;
+namespace CLRProfiler
+{
+	internal partial class DiffCallTreeForm : System.Windows.Forms.Form, IComparer, IDiffTreeOwner
+	{
+		
+
+		private System.Windows.Forms.Panel controlCollection;
+		
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		/// 
+		private System.ComponentModel.Container components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if(components != null)
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.controlCollection = new System.Windows.Forms.Panel();
+			this.SuspendLayout();
+			// 
+			// controlCollection
+			// 
+			this.controlCollection.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.controlCollection.Location = new System.Drawing.Point(0, 0);
+			this.controlCollection.Name = "controlCollection";
+			this.controlCollection.Size = new System.Drawing.Size(632, 446);
+			this.controlCollection.TabIndex = 0;
+			// 
+			// DiffCallTreeForm
+			// 
+			this.ClientSize = new System.Drawing.Size(632, 446);
+			this.Controls.Add(this.controlCollection);
+			this.Name = "DiffCallTreeForm";
+			this.Text = "DiffCallTreeForm";
+			this.Resize += new System.EventHandler(this.DiffCallTreeForm_Resize);
+			this.Closing += new System.ComponentModel.CancelEventHandler(this.DiffCallTreeForm_Closing);
+			this.ResumeLayout(false);
+
+		}
+		#endregion
+	}
+}
+
--- CLRProfiler/CLRProfiler/DiffCallTreeForm.cs
+++ CLRProfiler/CLRProfiler/DiffCallTreeForm.cs
@@ -23,7 +23,7 @@ namespace CLRProfiler
 	/// <summary>
 	/// Summary description for DiffCallTreeForm.
 	/// </summary>
-	internal class DiffCallTreeForm : System.Windows.Forms.Form, IComparer, IDiffTreeOwner
+	internal partial class DiffCallTreeForm : System.Windows.Forms.Form, IComparer, IDiffTreeOwner
 	{
 		
 		internal class SortingBehaviour
@@ -52,15 +52,6 @@ namespace CLRProfiler
 		internal CLRProfiler.DiffTreeListView diffCallTreeView;
 		private Rectangle formRect = new Rectangle( -1, -1, -1, -1 );
 		
-
-		private System.Windows.Forms.Panel controlCollection;
-		
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		/// 
-		private System.ComponentModel.Container components = null;
-		
 		public DiffCallTreeForm(TreeNodeBase root, AllocationDiff allocDiff)
 		{
 			this.Root = root;
@@ -68,52 +59,6 @@ namespace CLRProfiler
 			
 			InitForm();
 		}
-
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		protected override void Dispose( bool disposing )
-		{
-			if( disposing )
-			{
-				if(components != null)
-				{
-					components.Dispose();
-				}
-			}
-			base.Dispose( disposing );
-		}
-
-		#region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.controlCollection = new System.Windows.Forms.Panel();
-			this.SuspendLayout();
-			// 
-			// controlCollection
-			// 
-			this.controlCollection.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.controlCollection.Location = new System.Drawing.Point(0, 0);
-			this.controlCollection.Name = "controlCollection";
-			this.controlCollection.Size = new System.Drawing.Size(632, 446);
-			this.controlCollection.TabIndex = 0;
-			// 
-			// DiffCallTreeForm
-			// 
-			this.ClientSize = new System.Drawing.Size(632, 446);
-			this.Controls.Add(this.controlCollection);
-			this.Name = "DiffCallTreeForm";
-			this.Text = "DiffCallTreeForm";
-			this.Resize += new System.EventHandler(this.DiffCallTreeForm_Resize);
-			this.Closing += new System.ComponentModel.CancelEventHandler(this.DiffCallTreeForm_Closing);
-			this.ResumeLayout(false);
-
-		}
-		#endregion
 
 		
 		private void InitForm()
