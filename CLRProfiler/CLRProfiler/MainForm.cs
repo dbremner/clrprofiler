95fc6af5c75971ed082e134a2ccc858e78775566
David Bremner <dbremner@gmail.com>
2017-07-15 13:56:18 -0700

    suppress warnings

--- CLRProfiler/CLRProfiler/MainForm.cs
+++ CLRProfiler/CLRProfiler/MainForm.cs
@@ -2267,9 +2267,13 @@ namespace CLRProfiler
 
         struct SECURITY_ATTRIBUTES
         {
+#pragma warning disable 414
             public uint nLength;
+#pragma warning restore 414
             public IntPtr lpSecurityDescriptor;
+#pragma warning disable 414
             public int bInheritHandle;
+#pragma warning restore 414
         };
 
         [DllImport("Kernel32.dll", CharSet = CharSet.Auto)]
--- CLRProfiler/CLRProfiler/NamedManualResetEvent.cs
+++ CLRProfiler/CLRProfiler/NamedManualResetEvent.cs
@@ -62,9 +62,13 @@ namespace CLRProfiler
 
         struct SECURITY_ATTRIBUTES
         {
+#pragma warning disable 414
             public uint   nLength; 
+#pragma warning restore 414
             public IntPtr lpSecurityDescriptor; 
+#pragma warning disable 414
             public int    bInheritHandle; 
+#pragma warning restore 414
         }; 
 
         public NamedManualResetEvent(string eventName, bool initialState, bool createEvent)
