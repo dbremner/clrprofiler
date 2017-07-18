85a66dd6001c6ea454d2dda504272405c3f52c87
David Bremner <dbremner@gmail.com>
2017-07-17 19:26:49 -0700

    use appropriate type

--- CLRProfiler/CLRProfiler/SECURITY_ATTRIBUTES.cs
+++ CLRProfiler/CLRProfiler/SECURITY_ATTRIBUTES.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace CLRProfiler
@@ -12,7 +13,8 @@ namespace CLRProfiler
 #pragma warning restore 414
         public IntPtr lpSecurityDescriptor;
 #pragma warning disable 414
-        public int bInheritHandle;
+        [MarshalAs(UnmanagedType.Bool)]
+        public bool bInheritHandle;
 #pragma warning restore 414
     }
 }
