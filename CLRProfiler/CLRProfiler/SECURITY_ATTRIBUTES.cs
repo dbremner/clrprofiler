cd6705fceb9301cd852f1bd5c96028639408883a
David Bremner <dbremner@gmail.com>
2017-07-17 19:28:10 -0700

    add UsedImplicitly attribute

--- CLRProfiler/CLRProfiler/SECURITY_ATTRIBUTES.cs
+++ CLRProfiler/CLRProfiler/SECURITY_ATTRIBUTES.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using JetBrains.Annotations;
 
 namespace CLRProfiler
 {
     struct SECURITY_ATTRIBUTES
     {
 #pragma warning disable 414
+        [UsedImplicitly]
         public int nLength;
 #pragma warning restore 414
         public IntPtr lpSecurityDescriptor;
 #pragma warning disable 414
+        [UsedImplicitly]
         [MarshalAs(UnmanagedType.Bool)]
         public bool bInheritHandle;
 #pragma warning restore 414
