23fabfc76294bb4090022b623dfaa45a91c18ffe
David Bremner <dbremner@gmail.com>
2017-07-18 20:41:28 -0700

    extract two nested classes to separate files

--- CLRProfiler/CLRProfiler/TimeLineViewForm.cs
+++ CLRProfiler/CLRProfiler/TimeLineViewForm.cs
@@ -107,39 +107,6 @@ namespace CLRProfiler
 
         }
 
-        class TypeDesc : IComparable
-        {
-            internal readonly string typeName;
-            internal long totalSize;
-            internal Color color;
-            internal Brush brush;
-            internal Pen pen;
-            internal bool selected;
-            internal Rectangle rect;
-
-            internal TypeDesc(string typeName)
-            {
-                this.typeName = typeName;
-            }
-
-            public int CompareTo(Object o)
-            {
-                var t = (TypeDesc)o;
-                if (t.totalSize < this.totalSize)
-                {
-                    return -1;
-                }
-                else if (t.totalSize > this.totalSize)
-                {
-                    return 1;
-                }
-                else
-                {
-                    return 0;
-                }
-            }
-        }
-
         TypeDesc[] typeIndexToTypeDesc;
 
         ArrayList sortedTypeTable;
--- CLRProfiler/CLRProfiler/ViewByAddressForm.cs
+++ CLRProfiler/CLRProfiler/ViewByAddressForm.cs
@@ -96,55 +96,6 @@ namespace CLRProfiler
         AddressRange rangeList;
         int rangeCount = 0;
 
-        class TypeDesc : IComparable
-        {
-            internal readonly string typeName;
-            internal readonly int typeIndex;
-            internal ulong totalSize;
-            internal ulong selectedSize;
-            internal double percentage;
-            internal double selectedPercentage;
-            internal Color[] colors;
-            internal Brush[] brushes;
-            internal Pen[] pens;
-            internal int selected;
-            internal Rectangle rect;
-
-            internal TypeDesc(int typeIndex, string typeName)
-            {
-                this.typeIndex = typeIndex;
-                this.typeName = typeName;
-            }
-
-            public int CompareTo(Object o)
-            {
-                var t = (TypeDesc)o;
-                if (t.selectedSize != this.selectedSize)
-                {
-                    if (t.selectedSize < this.selectedSize)
-                    {
-                        return -1;
-                    }
-                    else
-                    {
-                        return 1;
-                    }
-                }
-                if (t.totalSize < this.totalSize)
-                {
-                    return -1;
-                }
-                else if (t.totalSize > this.totalSize)
-                {
-                    return 1;
-                }
-                else
-                {
-                    return 0;
-                }
-            }
-        }
-
         TypeDesc[] typeIndexToTypeDesc;
         string[] typeName;
 
